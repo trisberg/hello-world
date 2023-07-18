2299246b2ba7d5e5dcfc45785011085eda93a169
Thomas Risberg <trisberg@pivotal.io>
2023-07-17 22:44:22 -0400

    Move docker files to docker dir

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -e
 
 ./mvnw clean package
 BUILD_VERSION=$(cat VERSION)
-docker build --platform linux/amd64 -t springdeveloper/hello-world:$BUILD_VERSION .
+docker build --platform linux/amd64 -t springdeveloper/hello-world:$BUILD_VERSION -f ./docker/Dockerfile .
 docker push springdeveloper/hello-world:$BUILD_VERSION
