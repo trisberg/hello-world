ec406e883bccfe21895443e2ae4c1db49c7b0c98
trisberg <risbergt@vmware.com>
2023-07-18 11:48:52 -0400

    Remove need for priveleged

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,8 @@ mkdir -p ${CRAC_FILES_DIR}
 cd /home/app
 if [ -z "$(ls -A $CRAC_FILES_DIR)" ]; then
   echo "Save checkpoint to $CRAC_FILES_DIR"
-  echo 128 > /proc/sys/kernel/ns_last_pid; java -Dmanagement.endpoint.health.probes.add-additional-paths="true" -Dmanagement.health.probes.enabled="true" -XX:CRaCCheckpointTo=$CRAC_FILES_DIR org.springframework.boot.loader.JarLauncher &
+  ( echo 128 > /proc/sys/kernel/ns_last_pid ) 2>/dev/null || while [ $(cat /proc/sys/kernel/ns_last_pid) -lt 128 ]; do :; done
+  java -Dmanagement.endpoint.health.probes.add-additional-paths="true" -Dmanagement.health.probes.enabled="true" -XX:CRaCCheckpointTo=$CRAC_FILES_DIR org.springframework.boot.loader.JarLauncher &
   sleep 10
   jcmd org.springframework.boot.loader.JarLauncher JDK.checkpoint
   sleep 5
